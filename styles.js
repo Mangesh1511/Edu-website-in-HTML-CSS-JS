26aa30f760503940b96e0e077853cffad36637f2
Mangesh1511 <manpatil1451@gmail.com>
2022-05-23 20:57:44 +0530

    initial commit

---
+++ styles.js
@@ -0,0 +1,12 @@
+var menubutton = document.getElementById("menubutton");
+var sidenav = document.getElementById("sidenav");
+sidenav.style.right = "-250px";
+menubutton.onclick = function () {
+    if (sidenav.style.right == "-250px") {
+        sidenav.style.right = "0";
+    }
+    else {
+        sidenav.style.right = "-250px";
+    }
+}
+var scroll = new SmoothScroll('a[href*="#"]',{ speed:1000,speedAsDuration:true});
