7c8c63f9200d6292504aba4ce4a72ec9ec5fe66c
Mangesh <91506828+Mangesh1511@users.noreply.github.com>
2026-09-19 15:59:34 +0530

    Refresh cache for v5.0 Indian soundtrack engine

--- dadas-pr-studio/sw.js
+++ dadas-pr-studio/sw.js
@@ -1,4 +1,4 @@
-const CACHE='dadas-pr-studio-v49-20260919';
+const CACHE='dadas-pr-studio-v50-20260919';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./ai-engine.js'];
 
 self.addEventListener('install',event=>{
